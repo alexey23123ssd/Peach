08a39414ec80184acd05e23ee6d514ba0f57ff7d
Викторов Павел <decidedtosolvethisproblem@gmail.com>
2025-04-22 19:14:22 +0300

    Added onClick event listener on img in modal.jsx

--- src/components/UI/modal.jsx
+++ src/components/UI/modal.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Modal = ({regionsArr,citiesArr,isPressed,setIsPressed}) => {
+const Modal = ({regionsArr,citiesArr,isPressed}) => {
+    const [isClicked, setIsClicked] = useState(false);
     let pressed = "none";
     if(isPressed){
         pressed = "flex";
     }
+    const handleClick = (event) => {
+        let item;
+        if(isClicked){
+            setIsClicked(false);
+            event.target.src = "src/img/Polygon 13.svg"
+            item=event.target.closest(".map-section__modal-item");
+            item.querySelector(".map-section__modal-item__list").style.display = "block";
+        }
+        else {
+            setIsClicked(true);
+            event.target.src = "src/img/Polygon 8.svg"
+            item=event.target.closest(".map-section__modal-item");
+            item.querySelector(".map-section__modal-item__list").style.display = "none";
+        }
+
+    }
     return (
-        <div className="map-section__modal" style={{display:`${pressed}`}} onClick={() => setIsPressed(false)}>
+        <div className="map-section__modal" style={{display:`${pressed}`}}>
             <div className="map-section__modal-wrapper">
                     {regionsArr.map(item => item === "Все"? "" :
                         <div className="map-section__modal-item">
                             <div className="map-section__modal-itemCont">
                                 <h3 className="map-section__modal-item__heading">{item}</h3>
-                                <svg className="map-section__modal-item__arrow" width="8" height="4" viewBox="0 0 8 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M4 4L7.4641 0.25H0.535898L4 4Z" fill="#444444"/>
-                                </svg>
-
+                                <img src="src/img/Polygon 8.svg" alt="" onClick={handleClick} className="map-section__modal-item__arrow"/>
                             </div>
 
-                            <ul className="map-section__modal-item__list">
+                            <ul  className="map-section__modal-item__list">
                                 {citiesArr.map((city,index) => city.region===item ?
                                     <li className="map-section__modal-item__list-item" key={index}>
                                         {city.name}
